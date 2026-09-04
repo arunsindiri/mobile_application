6295a1957c8e56958c4d336cae7c4026c19de556
arun <arunsindiri123@gmail.com>
2026-09-04 05:54:05 +0000

    feat: connect FastAPI to PostgreSQL

---
+++ backend/app/database/connection.py
@@ -0,0 +1,13 @@
+import os 
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+with engine.connect() as connection:
+    print("Database connection successfull")
